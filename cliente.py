fe32720dae35761ae9829243bf68d98eeb8cd4e9
matuqueri <matuqueri_2014@hotmail.com.ar>
2025-11-24 01:22:03 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -1,1 +1,14 @@
+class Cliente:
+    def __init__(self, id, nombre, telefono):
+        
+        self.id = id
+        
+        
+        self.nombre = nombre
+        
+        
+        self.telefono = telefono
 
+    def __str__(self):
+        
+        return f"{self.id} - {self.nombre} ({self.telefono})"
