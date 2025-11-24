412920cf9a3b943ac4dcef2c190fda2fae14c8b5
matuqueri <matuqueri_2014@hotmail.com.ar>
2025-11-24 02:07:09 -0300

    Update turno.py

--- turno.py
+++ turno.py
@@ -1,1 +1,19 @@
+class Turno:
+    def __init__(self, id, cliente_id, peluquero_id, fecha, hora):
+        
+        self.id = id
+        
+        
+        self.cliente_id = cliente_id
+        
+        
+        self.peluquero_id = peluquero_id
+        
+        
+        self.fecha = fecha
+        
+        
+        self.hora = hora
 
+    def __str__(self):
+        return f"Turno {self.id}: Cliente {self.cliente_id}, Peluquero {self.peluquero_id}, {self.fecha} {self.hora}"
