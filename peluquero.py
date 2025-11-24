8d955697a544247990210e130a1545b9b993f22d
matuqueri <matuqueri_2014@hotmail.com.ar>
2025-11-24 01:44:56 -0300

    Update peluquero.py

--- peluquero.py
+++ peluquero.py
@@ -1,1 +1,10 @@
+class Peluquero:
+    def __init__(self, id, nombre):
+        
+        self.id = id
+        
+        
+        self.nombre = nombre
 
+    def __str__(self):
+        return f"{self.id} - {self.nombre}"
